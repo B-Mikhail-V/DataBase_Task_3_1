e2b5e33e09d41838288e04e41ab2c180f866e6ee
B-Mikhail-V <bakulin.mv@gmail.com>
2022-01-27 22:48:43 +0300

    Code_2

--- Select_Training_2.py
+++ Select_Training_2.py
@@ -7,7 +7,7 @@ import pandas as pd
 # engine = sqlalchemy.create_engine('postgresql://mikhail:123456@localhost:5432/dvdrental')
 from pandas import DataFrame
 
-engine = sqlalchemy.create_engine('postgresql://mikhail:123456@localhost:5432/musik_1')
+engine = sqlalchemy.create_engine('postgresql://mikhail:123456@localhost:5432/muzic_2')
 
 
 connection = engine.connect()
@@ -37,14 +37,18 @@ connection = engine.connect()
 # ('Peter Gabriel')
 #
 # # """)
-table_dict = {}
-table_list = ['singers', 'genres', 'albums', 'tracks', 'collections', 'collectionstracks', 'singersalbums', 'singersgenres']
-sheets_list = ['List_1', 'List_2', 'List_3', 'List_4', 'List_5', 'List_6', 'List_7', 'List_8']
-for i in range(5):
+df1: DataFrame = pd.read_excel("For Database_25_01_22.xlsx", 'tables', index_col=False)
+table_list = df1.values.tolist()
+print(table_list)
 
-    df: DataFrame = pd.read_excel("For Database_25_01_22.xlsx", sheets_list[i], index_col=0)
-    print(df)
-    df.to_sql(table_list[i], con=engine, if_exists='append', index=False)
+# table_dict = {}
+# table_list = ['singers', 'genres', 'albums', 'tracks', 'collections', 'collectionstracks', 'singersalbums', 'singersgenres']
+# sheets_list = ['List_1', 'List_2', 'List_3', 'List_4', 'List_5', 'List_6', 'List_7', 'List_8']
+
+for i in range(3,4):
+    df: DataFrame = pd.read_excel("For Database_25_01_22.xlsx", table_list[i][0], index_col=0)
+    # print(df)
+    df.to_sql(table_list[i][0], con=engine, if_exists='append', index=False)
 
 # conn = connect_to_db(DB_FILE_PATH)
 # df.to_sql(singers, conn, index=False)
