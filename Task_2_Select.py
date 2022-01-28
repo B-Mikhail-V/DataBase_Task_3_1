05703ea95295a0c057f2f44c24a44dcd935617cf
Mikail Bakulin <bakulin.mv@gmail.com>
2022-01-28 15:16:37 +0300

    Insertion&Select codes

--- Task_2_Select.py
+++ Task_2_Select.py
@@ -1,26 +1,11 @@
-import psycopg2
+
 import sqlalchemy
-import pandas as pd
-# from pandas import openpyxl
 
-
-# engine = sqlalchemy.create_engine('postgresql://mikhail:123456@localhost:5432/dvdrental')
-from pandas import DataFrame
 
 engine = sqlalchemy.create_engine('postgresql://mikhail:123456@localhost:5432/musik_1')
 
 
 connection = engine.connect()
-
-# df1: DataFrame = pd.read_excel("For Database_25_01_22.xlsx", 'tables', index_col=False)
-# table_list = df1.values.tolist()
-# # print(table_list)
-#
-#
-# for i in range(len(table_list)):
-#     df: DataFrame = pd.read_excel("For Database_25_01_22.xlsx", table_list[i][0], index_col=0)
-#     # print(df)
-#     df.to_sql(table_list[i][0], con=engine, if_exists='append', index=False)
 
 sel = connection.execute("""
 SELECT * FROM singers;
